8ca94b318ac1994332ed3054e0cba035e056569f
lowskydev <lowskydev@gmail.com>
2026-04-28 12:51:05 +0200

    during the class lab06

---
+++ lab06/orion-system/scripts/telemetry_sender.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+HOST="127.0.0.1"
+PORT=5000
+SAT_ID="sat-001"
+
+echo "=== TELEMETRY SENDER STARTED ==="
+echo "Sending to $HOST:$PORT"
+echo ""
+
+while true; do
+    TS=$(date -Iseconds)
+    VALUE=$((RANDOM % 100))
+    MESSAGE="SAT_ID=$SAT_ID;TIMESTAMP=$TS;VALUE=$VALUE"
+
+    echo "[SENT] $MESSAGE"
+
+    echo "$MESSAGE" | nc -w 1 "$HOST" "$PORT"
+
+    sleep 2
+done
